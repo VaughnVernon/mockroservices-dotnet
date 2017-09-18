3909218f69ac58878b7ba0f56914ae83963c1d41
Vaughn Vernon <vvernon@shiftmethod.com>
2017-09-17 23:40:02 -0700

    Edited assembly description for content.

--- mockroservices-dotnet/Properties/AssemblyInfo.cs
+++ mockroservices-dotnet/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("mockroservices-dotnet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Toolkit for creating microservices in a multi-project test environment using mock distributed mechanisms.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("vaughnvernon.co")]
 [assembly: AssemblyProduct("mockroservices-dotnet")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
